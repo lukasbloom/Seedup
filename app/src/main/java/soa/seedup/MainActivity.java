b8a56e4a452b6082d505823c1a55d64b75f3e8f1
Luk <lukasbloom@gmail.com>
2017-06-19 01:52:27 -0300

    Icono de aplicacion

--- app/src/main/java/soa/seedup/MainActivity.java
+++ app/src/main/java/soa/seedup/MainActivity.java
@@ -13,7 +13,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.bluetooth.BluetoothAdapter;
-import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.widget.Button;
 import android.widget.CompoundButton;
